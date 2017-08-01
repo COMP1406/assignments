42ec1a1bb5d56ff55e92e0a0917c35d5a280064f
COMP1406/1006 <comp1406.cu@gmail.com>
2017-08-01 11:58:04 -0400

    Add files via upload

---
+++ a6/Balanced.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Balanced{
+  
+  
+  /** 
+	 Checks if a string is balanced or not.
+     
+   @param s is a non-null string
+   @return true of the input <code>s</code> is balanced and false otherwise.
+  **/
+  public static boolean isBalanced(String s){
+    return false;
+  }
+  
+  /** 
+	 Counts the number of balanced strings in the input array.
+	 
+	 @param in is a non-null array of strings
+   @return the number of strings in the input <code>in</code> that are balanced.
+  **/
+  public static int numberOfBalancedStrings(String[] in){
+    return -1;
+  }
+  
+  
+}
